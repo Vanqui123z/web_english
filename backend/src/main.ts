e11f057122a566b1210f7a30e51f794ea806b43d
VanQui <levanquy1923@gmail.com>
2025-08-24 22:26:09 +0700

    update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,15 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import compression from 'compression';
-
-// compression : (nén response)
+import { join } from 'path';
+import * as express from 'express';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-    app.enableCors();
-    app.use(compression()); 
+
+  // Serve static files từ public
+  app.use(express.static(join(__dirname, '..', 'public')));
+
+  // Nếu không match API nào thì trả index.html
+  app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    if (req.path.startsWith('/api')) {
+      return next(); // cho request API đi tiếp
+    }
+    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+  });
+
+  app.enableCors();
+  app.use(compression());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
